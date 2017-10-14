0af0d4951e7735ab34db11d69441b9d08facf06e
Adrian Santos <adriansantos31@gmail.com>
2017-10-14 10:33:01 -0400

    Finished the Game
    Added obstacles, update frame and physics engine

---
+++ FlappyBirt/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// The Target
+	public Transform target;
+
+
+	void Update () {
+		transform.position = new Vector3(target.position.x,
+			transform.position.y,
+			transform.position.z);
+	}
+
+	void LateUpdate () {
+		transform.position = new Vector3(target.position.x,
+			transform.position.y,
+			transform.position.z);
+	}
+}
